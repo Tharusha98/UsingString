ce2c2d3759ce65f09fac533931fe431eda366376
Tharusha98 <68142354+Tharusha98@users.noreply.github.com>
2020-08-02 23:13:10 +0530

    Second Commit

--- app/src/main/java/com/example/usingstring/ActivityOne.java
+++ app/src/main/java/com/example/usingstring/ActivityOne.java
@@ -3,6 +3,7 @@ package com.example.usingstring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityOne extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ActivityOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        TextView txtView2;
+        txtView2 = findViewById(R.id.textView2);
+        txtView2.setText(R.string.Msg2);
     }
 }
